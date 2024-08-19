46071a36249bda27993c8d2078438f1ae36e77ee
Jos de Jong <wjosdejong@gmail.com>
2024-08-19 20:12:32 +0200

    chore: remove unused types

--- src/types.ts
+++ src/types.ts
@@ -2,27 +2,6 @@ export type JSONPrimitive = number | boolean | null
 
 export type JSONProperty = string | string[]
 
-export type JSONFilterCondition =
-  | [left: JSONFilterCondition, 'and' | 'or', right: JSONFilterCondition]
-  | [left: JSONProperty, op: JSONFilterOperatorName, right: JSONPrimitive]
-  | [left: JSONProperty, op: JSONFilterOperatorName, right: JSONProperty]
-  | [
-      left: number | boolean | null | ['string', string],
-      op: JSONFilterOperatorName,
-      right: JSONProperty
-    ]
-
-export type JSONFilterOperatorName =
-  | '=='
-  | '>'
-  | '>='
-  | '<'
-  | '<='
-  | '!='
-  | 'in'
-  | 'not in'
-  | 'regex'
-
 export type JSONQueryFunction = [name: string, ...args: unknown[]]
 export type JSONQueryOperator = [left: string, op: string, ...right: unknown[]]
 export type JSONQueryArray = JSONQuery[]
