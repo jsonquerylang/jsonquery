823a3bc57d555f50f7ce68b300351869732f57a3
Jos de Jong <wjosdejong@gmail.com>
2024-08-28 12:56:00 +0200

    chore: validate the bundle output

---
+++ test-lib/apps/esmApp.mjs
@@ -0,0 +1,18 @@
+import { jsonquery } from '../../lib/jsonquery.js'
+
+const data = [
+  { name: 'Chris', age: 23, city: 'New York' },
+  { name: 'Emily', age: 19, city: 'Atlanta' },
+  { name: 'Joe', age: 32, city: 'New York' },
+  { name: 'Kevin', age: 19, city: 'Atlanta' },
+  { name: 'Michelle', age: 27, city: 'Los Angeles' },
+  { name: 'Robert', age: 45, city: 'Manhattan' },
+  { name: 'Sarah', age: 31, city: 'New York' }
+]
+
+const result = jsonquery(data, [
+  ['filter', ['city', '==', 'New York']],
+  ['map', ['name']]
+])
+
+console.log(JSON.stringify(result))
---
+++ test-lib/lib.test.js
@@ -0,0 +1,27 @@
+import cp from 'node:child_process'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { describe, test, expect } from 'vitest'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+describe('lib', () => {
+  test('should load the library using ESM', async () => {
+    const filename = join(__dirname, 'apps/esmApp.mjs')
+    const result = await run(`node ${filename}`)
+    expect(result).toBe('["Chris","Joe","Sarah"]\n')
+  })
+})
+
+function run(command) {
+  return new Promise((resolve, reject) => {
+    cp.exec(command, function (error, result) {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
