041979100ee1fe1a2a83c1e1e1704801246b0ac6
Jos de Jong <wjosdejong@gmail.com>
2024-11-08 21:57:35 +0100

    chore: move running the compile test-suite into compile.test.ts

--- src/compile.test.ts
+++ src/compile.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from 'vitest'
+import suite from '../test-suite/compile.json'
 import { compile } from './compile'
 import { buildFunction } from './functions'
 import type { JSONQuery, JSONQueryCompileOptions } from './types'
@@ -941,4 +942,60 @@ describe('compile', () => {
       WashingtonCities: 'Bellevue, Olympia, Seattle'
     })
   })
+})
+
+describe('test-suite', () => {
+  interface DataTest {
+    category: string
+    description: string
+    data: unknown
+    query: JSONQuery
+    output: unknown
+  }
+
+  interface DataRefTest {
+    category: string
+    description: string
+    dataRef: string
+    query: JSONQuery
+    output: unknown
+  }
+
+  interface CompileTestSuite {
+    updated: string
+    dataRefs: Record<string, unknown>
+    tests: Array<DataTest | DataRefTest>
+  }
+
+  function isDataRefTest(test: unknown): test is DataRefTest {
+    return test && typeof (test as Record<string, unknown>).dataRef === 'string'
+  }
+
+  const groupByCategory = compile(['groupBy', ['get', 'category']])
+  const testsByCategory = groupByCategory(suite.tests) as Record<string, CompileTestSuite['tests']>
+
+  for (const [category, tests] of Object.entries(testsByCategory)) {
+    describe(category, () => {
+      for (const currentTest of tests) {
+        if (isDataRefTest(currentTest)) {
+          const { description, dataRef, query, output } = currentTest
+
+          test(description, () => {
+            const resolvedData = suite.dataRefs[dataRef]
+            const actualOutput = compile(query)(resolvedData)
+
+            expect({ dataRef, query, output: actualOutput }).toEqual({ dataRef, query, output })
+          })
+        } else {
+          const { description, data, query, output } = currentTest
+
+          test(description, () => {
+            const actualOutput = compile(query)(data)
+
+            expect({ data, query, output: actualOutput }).toEqual({ data, query, output })
+          })
+        }
+      }
+    })
+  }
 })
