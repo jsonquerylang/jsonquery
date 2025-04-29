800f1ee7045e5a3d443f2aeccb858cb309978ba7
Jos de Jong <wjosdejong@gmail.com>
2025-04-29 10:52:36 +0200

    chore: make `parse` function agnostic (#19)

--- src/parse.ts
+++ src/parse.ts
@@ -1,4 +1,3 @@
-import { functions } from './functions'
 import { extendOperators, leftAssociativeOperators, operators, varargOperators } from './operators'
 import {
   startsWithIntRegex,
@@ -27,7 +26,6 @@ import type { JSONQuery, JSONQueryParseOptions, OperatorGroup } from './types'
  */
 export function parse(query: string, options?: JSONQueryParseOptions): JSONQuery {
   const customOperators = options?.operators ?? []
-  const allFunctions = { ...functions, ...options?.functions }
   const allOperators = extendOperators(operators, customOperators)
   const allOperatorsMap = Object.assign({}, ...allOperators)
   const allVarargOperators = varargOperators.concat(
@@ -134,10 +132,6 @@ export function parse(query: string, options?: JSONQueryParseOptions): JSONQuery
       return parseObject()
     }
     i++
-
-    if (!allFunctions[name]) {
-      throwError(`Unknown function '${name}'`)
-    }
 
     skipWhitespace()
 
