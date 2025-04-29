36678da1b5ffcf2198243df2e6391e44288a5a3b
Jos de Jong <wjosdejong@gmail.com>
2025-04-29 10:57:58 +0200

    feat: deep equal support (#20)

--- src/is.ts
+++ src/is.ts
@@ -4,3 +4,18 @@ export const isObject = (value: unknown): value is object =>
   value && typeof value === 'object' && !isArray(value)
 
 export const isString = (value: unknown): value is string => typeof value === 'string'
+
+// source: https://stackoverflow.com/a/77278013/1262753
+export const isEqual = <T>(a: T, b: T): boolean => {
+  if (a === b) {
+    return true
+  }
+
+  const bothObject = a && b && typeof a === 'object' && typeof b === 'object'
+
+  return (
+    bothObject &&
+    Object.keys(a).length === Object.keys(b).length &&
+    Object.entries(a).every(([k, v]) => isEqual(v, b[k as keyof T]))
+  )
+}
