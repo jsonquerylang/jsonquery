893df527f5de925a68b41e947d587eef6c1373e6
Christophe Eymard <ceymard@pm.me>
2026-06-22 11:59:52 +0200

    allow unicode properties in unquoted identifiers

--- src/regexps.ts
+++ src/regexps.ts
@@ -1,5 +1,5 @@
-export const unquotedPropertyRegex = /^[a-zA-Z_$][a-zA-Z\d_$]*$/
-export const startsWithUnquotedPropertyRegex = /^[a-zA-Z_$][a-zA-Z\d_$]*/
+export const unquotedPropertyRegex = /^[\p{L}\p{Nl}_][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}_$]*$/u
+export const startsWithUnquotedPropertyRegex = /^[\p{L}\p{Nl}_][\p{L}\p{Nl}\p{Nd}\p{Mn}\p{Mc}\p{Pc}_$]*/u
 export const startsWithStringRegex = /^"(?:[^"\\]|\\.)*"/ // https://stackoverflow.com/a/249937/1262753
 export const startsWithNumberRegex = /^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/ // https://stackoverflow.com/a/13340826/1262753
 export const startsWithIntRegex = /^(0|[1-9][0-9]*)/
